be1e40376c4af09e32ca060ebfec98a0c0eaf58f
nitin-das <nitin.das@thomson.co.uk>
2020-03-13 16:24:19 +0000

    Modified Code

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "id" {
+  value = aws_nat_gateway.nat_gateway.id
+}
+
+output "allocation_id" {
+  value = aws_nat_gateway.nat_gateway.allocation_id
+}
+
+output "network_interface_id" {
+  value = aws_nat_gateway.nat_gateway.network_interface_id
+}
+
+output "public_ip" {
+  value = aws_nat_gateway.nat_gateway.public_ip
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "tags" {
+  description = "A mapping of tags to assign to the resource."
+  type        = map(string)
+}
+
+variable "allocation_id" {
+  description = "The Allocation ID of the Elastic IP address for the gateway"
+}
+
+variable "subnet_id" {
+  description = "The Subnet ID of the subnet in which to place the gateway"
+}
